70238e35c17c20576ca49893f09af477fc69748e
soom12 <soo.m@northeastern.edu>
2024-05-13 13:19:45 -0400

    updates to blast script

--- scripts/4_blast_fa.sh
+++ scripts/4_blast_fa.sh
@@ -4,7 +4,25 @@
 # `module load anaconda3`
 # `conda activate /work/geisingerlab/conda_env/blast_corr`
 
-blastp -query $1 -db $2 \
-    -max_target_seqs 1 \
-    -outfmt 6 -evalue 1e-5 -num_threads 4
+module load anaconda3
+source /shared/centos7/anaconda3/2021.05/etc/profile.d/conda.sh
+conda activate /work/geisingerlab/conda_env/blast_corr
 
+source ./config.cfg
+
+mkdir -p $BLAST_OUTPUT
+
+OUTFILE=${BLAST_OUTPUT}/blast_output
+
+echo "Query fasta files" "${QUERY_FASTAS[@]}"
+echo "Blastdb for reference genome: $OUTPUT_DATABASE"
+echo "Saving BLAST output to: $OUTFILE"
+
+echo "merging query fastas"
+concatenated_queries=$(cat "${QUERY_FASTAS[@]}")
+
+
+blastp -query $concatenated_queries -db $OUTPUT_DATABASE \
+    -max_target_seqs 1 \
+    -outfmt 6 -evalue 1e-5 -num_threads 4 \
+    1>$OUTFILE
